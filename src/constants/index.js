0defc339a978bdf27396fff715c45dc0e7f0a886
ampaire <femayapr@gmail.com>
2020-09-26 14:56:07 -0600

    Add the action for creating a new product by the admin

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,6 @@
+/* eslint-disable no-plusplus */
+/* eslint-disable guard-for-in */
+/* eslint-disable no-restricted-syntax */
 // API url
 export const apiUrl = 'https://beautiful-tags.herokuapp.com/';
 
@@ -19,4 +22,41 @@ export const authConstants = {
   login_user_fail: 'login_user_failure',
   login_user_success: 'login_user_successfull',
   logout_user: 'logout_user_request',
+};
+
+const navLinks = document.getElementsByClassName('nav-links');
+
+export const toggle = () => {
+  navLinks[0].classList.toggle('open');
+};
+
+export const inputValidation = ({ message }) => {
+  const invalid = [];
+  const general = message.split(':');
+  const specific = general[1].split(',');
+  for (const i in specific) {
+    invalid.push(specific[i].split(' ')[1].toLowerCase());
+  }
+  const inputs = document.querySelectorAll('input,textArea');
+  for (const i in inputs) {
+    if (invalid.includes(inputs[i].name)) {
+      inputs[i].className = `${inputs[i].className} is-danger`;
+    } else {
+      inputs[i].className = `${inputs[i].className} is-success`;
+    }
+  }
+};
+
+export const loadingIcon = params => {
+  const className = params || 'is-disabled button is-loading';
+  const buttons = document.querySelectorAll('button');
+  for (let i = 0; i < buttons.length; i++) {
+    const arr = buttons[i].className.split(' ');
+    if (arr[(arr.length) - 1] === 'is-loading') {
+      arr.pop();
+      buttons[i].className = arr.join(' ');
+    } else {
+      buttons[i].className = `${buttons[i].className} ${className}`;
+    }
+  }
 };
